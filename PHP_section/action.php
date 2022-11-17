25ccdd79773562f63f7dba0f17040c9d00a9eeda
Ctibor Kovalčík <ckkoval33@gmail.com>
2022-11-17 14:52:40 +0100

    first commit

---
+++ PHP_section/action.php
@@ -0,0 +1,61 @@
+<?php
+
+
+
+function log_user($date,$user,$delayed) {
+  $file = "logfile.log";
+  $data = $date->format('Y-m-d H:i:s') . " | ".$user." | ";
+  file_put_contents($file, $data, FILE_APPEND);
+  if ($delayed) {
+    add_delay($file);
+  }
+  file_put_contents($file, PHP_EOL, FILE_APPEND);
+}
+
+function add_delay($file){
+  file_put_contents($file, "Student was deleyed",FILE_APPEND);
+}
+
+
+function IsDelayed($now){
+  $delay = new DateTime("08:00:00");
+  if ($now > $delay) {
+    return true;
+  }
+  return false;
+}
+
+function getlog(){
+  $file = "logfile.log";
+  $log = file_get_contents($file);
+  echo $log;
+}
+
+if(array_key_exists('Log', $_POST)) {
+  getLog();
+}
+
+if (isset($_POST['username'])) {
+  $user = $_POST['username'];
+}
+else{
+  $user = $_GET['username'];
+}
+
+if ($user){
+  $date = new DateTime();
+
+  if ($date > new DateTime("20:00")){
+    return die("You are late");
+  }
+
+  $delayed = IsDelayed($date);
+
+  log_user($date,$user,$delayed);
+}
+
+
+
+
+
+?>
---
+++ PHP_section/index.php
@@ -0,0 +1,41 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+
+
+
+
+
+<!--- 
+form with POST method
+-->
+<h1>
+  Loggger
+</h1>
+<div>
+<form action="" method="POST">
+  <input type="text" name="username" placeholder="Enter your name">
+  <button type="submit">Submit</button>
+  <input type="submit" name="Log" value="Log">
+  
+</form>
+
+</div>
+
+
+<?php include "./action.php";?>
+
+
+  
+
+
+
+</body>
+</html>
