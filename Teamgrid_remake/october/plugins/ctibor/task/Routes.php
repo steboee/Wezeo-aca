a8064c7438c5ab97a88276355bc03fa7a38c680f
Ctibor Kovalčík <ckkoval33@gmail.com>
2022-11-23 09:31:08 +0100

    typos

--- Teamgrid_remake/october/plugins/ctibor/task/Routes.php
+++ Teamgrid_remake/october/plugins/ctibor/task/Routes.php
@@ -2,22 +2,22 @@
 
 use Backend\Classes\Controller;
 
-use ctibor\task\http\controllers\TimeTrackerController;
-use ctibor\task\Http\Controllers\TasksController;
-use ctibor\task\Task;
-use ctibor\Core\Config;
+use Ctibor\Task\Http\Controllers\TimeTrackerController;
+use Ctibor\Task\Http\Controllers\TasksController;
+use Ctibor\Task\Task;
+use Ctibor\Core\Config;
 use WUserApi\UserApi\Http\Middlewares;
 
 Route::prefix('api')->group(function () {
     // group routes by middleware
     Route::middleware('WUserApi\UserApi\Http\Middlewares\Authenticate')->group(function () {
-        Route::post('tasks', [TasksController::class, 'store']);
-        Route::get('tasks/{id}', [TasksController::class, 'show']);
-        Route::get('tasks', [TasksController::class, 'index']);
-        Route::put('tasks/{id}', [TasksController::class, 'update']);
-        Route::delete('tasks/{id}', [TasksController::class, 'delete']);
-        Route::post('tasks/{id}/start_tracking', [TimeTrackerController::class, 'start_tracking']);
-        Route::post('tasks/{id}/stop_tracking', [TimeTrackerController::class, 'stop_tracking']);
+        Route::post('Tasks', [TasksController::class, 'store']);
+        Route::get('Tasks/{id}', [TasksController::class, 'show']);
+        Route::get('Tasks', [TasksController::class, 'index']);
+        Route::put('Tasks/{id}', [TasksController::class, 'update']);
+        Route::delete('Tasks/{id}', [TasksController::class, 'delete']);
+        Route::post('Tasks/{id}/start_tracking', [TimeTrackerController::class, 'start_tracking']);
+        Route::post('Tasks/{id}/stop_tracking', [TimeTrackerController::class, 'stop_tracking']);
 
         Route::get("time_trackings", [TimeTrackerController::class, 'index']);
         Route::get("time_trackings/{id}", [TimeTrackerController::class, 'show']);
